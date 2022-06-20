205f0e46a15757c635e2cc3637ad7147f30aff2f
Fethi Çetin <fthi.cetin@gmail.com>
2022-06-21 00:56:40 +0300

    Update TweetRepository.java

--- TweetConsumer/src/main/java/com/tweetconsumer/repository/TweetRepository.java
+++ TweetConsumer/src/main/java/com/tweetconsumer/repository/TweetRepository.java
@@ -2,5 +2,6 @@ package com.tweetconsumer.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+@Repository
 public interface TweetRepository  extends MongoRepository<String,String> {
 }
